79c3d465bd15d29daa947218f6bf56a9247e219a
Kleverson Eller <kleverson.a16@gmail.com>
2023-03-01 03:11:35 -0300

    feat: :sparkles: Implementando funcs (printBoard, makeMove, isGameOver e printResults)
    Implementando funçoes para criação do jogo Tik-Tak-Toe

--- src/TikTakToe/TikTakToeGame.cs
+++ src/TikTakToe/TikTakToeGame.cs
@@ -2,28 +2,74 @@ namespace TikTakToe;
 
 public class TikTakToeGame
 {
-    public char[,] board = new char[3, 3] { { ' ', ' ', ' ' }, { ' ', ' ', ' ' }, { ' ', ' ', ' ' } };
+    public char[,] board = new char[3, 3] {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
     public char currentPlayer = 'x';
     public char winner = ' ';
 
     public void printBoard()
     {
-        throw new NotImplementedException();
+        for(int line=0; line < board.GetLength(0); line++){
+            string result = "";
+
+            for(int colum=0; colum < board.GetLength(1); colum++)
+            {
+                result += board[line, colum]+" ";
+            }
+
+            Console.Write(result);
+        }
     }
 
     public void makeMove(int line, int column, char player)
     {
-        throw new NotImplementedException();
+        board[line, column] = player;
     }
 
     public bool isGameOver()
     {
-        throw new NotImplementedException();
+        int playerXVDia = 0;
+        int playerXVDiaSec = 0;
+        int playerOVDia = 0;
+        int playerOVDiaSec = 0;
+        int isDraw = 0;
+
+        for(int line=0; line < board.GetLength(0); line++){
+            int playerXVert = 0;
+            int playerXHori = 0;
+            int playerOVert = 0;
+            int playerOHori = 0;
+
+            int lineReverse = board.GetLength(0) - (line + 1);
+
+            if(board[line, line] == 'x') playerXVDia += 1;
+            if(board[line, line] == 'o') playerOVDia += 1;
+            if(board[lineReverse, lineReverse] == 'x') playerXVDiaSec += 1;
+            if(board[lineReverse, lineReverse] == 'o') playerOVDiaSec += 1;
+
+            for(int colum=0; colum < board.GetLength(1); colum++)
+            {
+                if(board[line, colum] == 'x') playerXHori += 1;
+                if(board[line, colum] == 'o') playerOHori += 1;
+                if(board[colum, colum] == 'x') playerXVert += 1;
+                if(board[colum, colum] == 'o') playerOVert += 1;
+                if(board[line, colum] == ' ') isDraw += 1;
+            }
+
+            if(playerOHori == 3 || playerOVert == 3) winner = 'o';
+            if(playerXHori == 3 || playerXVert == 3) winner = 'x';
+        }
+
+        if(playerOVDia == 3 || playerOVDiaSec == 3) winner = 'o';
+        if(playerXVDia == 3 || playerXVDiaSec == 3) winner = 'x';
+        if(isDraw != 0) return false;
+        else return true;
     }
 
     public void printResults()
     {
-        throw new NotImplementedException();
+        if(winner == ' ') Console.WriteLine("Empate! Deu Velha!");
+        if(winner == 'x') Console.WriteLine("O jogador x venceu!");
+        if(winner == 'o') Console.WriteLine("O jogador o venceu!");
     }
 
     public char getCurrentPlayer()
